d5f9f07a1f608b60f77478ca2a707ef122c6703e
ektor5 <ek5.chimenti@gmail.com>
2016-07-27 11:41:55 +0200

    mac: Added endcmd, initcmd

--- mac/src/Samba.cpp
+++ mac/src/Samba.cpp
@@ -55,6 +55,7 @@ Samba::init()
     uint8_t cmd[3];
     uint32_t cid;
 
+    _port->initcmd();
     _port->timeout(TIMEOUT_QUICK);
 
     // Allows Arduino auto-reset
@@ -77,6 +78,7 @@ Samba::init()
         _port->read(cmd, 3);
     }
 
+    usleep(100);
     // Set binary mode
     if (_debug)
         printf("Set binary mode\n");
@@ -587,5 +589,7 @@ Samba::reset(void)
     // This delay is here to give the time to kernel driver to
     // sort out things before closing the port.
     usleep(100000);
+
+    _port->endcmd();  
 }
 
--- mac/src/SerialPort.h
+++ mac/src/SerialPort.h
@@ -49,6 +49,8 @@ public:
                       StopBit stop = StopBitOne) = 0;
     virtual void close() = 0;
 
+    virtual bool initcmd() = 0;
+    virtual bool endcmd() = 0;
     virtual bool isUsb() = 0;
 
     virtual int read(uint8_t* data, int size) = 0;
